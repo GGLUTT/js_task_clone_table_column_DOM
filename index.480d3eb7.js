56042102b8260891aadd7e895837377c581e1f9b
luter <newgglutt@gmail.com>
2024-06-28 13:45:05 +0300

    make build

---
+++ index.480d3eb7.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector("table"),t=e.querySelector("thead"),o=e.querySelector("tbody"),r=e.querySelector("tfoot"),n=function(e){e.querySelectorAll("tr").forEach(function(e){var t=e.querySelectorAll("th, td"),o=t[1].cloneNode(!0);e.insertBefore(o,t[4])})};n(t),n(o),n(r)});
+//# sourceMappingURL=index.480d3eb7.js.map
